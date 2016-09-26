175e93b6ccd31ee62e54b9671629fae6052d0028
unknown <mmule@ManjushaM.clarice.com>
2016-09-26 20:57:06 +0530

    New Method assignment modified

--- Manjusha_Mule/NewMethod.js
+++ Manjusha_Mule/NewMethod.js
@@ -1,31 +1,17 @@
-function Person(arr){
-	var obj = {};
 
-	for(var i =0 ; i < arr.length ; i++)
-	{
-		switch (i) {
-			case 0:
-				obj.firstName = arr[i];
-				break;
-			case 1:
-				obj.lastName = arr[i];
-				break;	
-			case 2:
-				obj.age = arr[i];
-				break;
-			case 3:
-				obj.address = arr[i];
-				break;		
-		}
-	}
-
-	return obj;
+function Person(fname, lname, age, address){
+	this.fname = fname;
+	this.lname = lname;
+	this.age = age;
+	this.address = address;
 }
+
 
 function NewMethod(){
 	var fun = arguments[0];
-	
-	return fun(arguments[1]);
+	var obj = {};
+	fun.apply(obj, arguments[1]);
+	return obj;
 }
 
 var person1 = NewMethod(Person,["Manjusha","Mule", 23, "Pune"]);
